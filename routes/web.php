043c2be34dba18e7a3b8ffd3d6aae63ff134139f
PicGigi <junior.1997@live.it>
2023-02-02 19:31:04 +0100

    Login Logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'Admin\HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+        Route::resource('posts', 'PostController');
+});
+
+
